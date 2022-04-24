46ac1a95ee6d65b41bd4fec418609c9ddff347f4
Matheus Moura <matheusmouraa@icloud.com>
2022-04-24 02:43:08 -0300

    Ajuste no JS

---
+++ ha/script.js
@@ -0,0 +1,53 @@
+// FASE 1 - Homem Aranha
+
+function confereResposta1(respostaCerta) {
+  var respostaJogador = prompt("Digite a resposta (A, B, ou C):");
+  var contador = 3;
+
+  while (contador >= 1) {
+    if (respostaJogador.toUpperCase() === respostaCerta) {
+      alert("Parabéns! Resposta correta!");
+      return (location = "./homemaranha1.html");
+    } else if (
+      respostaJogador.toUpperCase() === "A" ||
+      respostaJogador.toUpperCase() === "B"
+    ) {
+      contador--;
+      alert(
+        `Resposta incorreta, tente novamente!\n Tentativas restantes: ${contador}`
+      );
+      respostaJogador = prompt("Digite a resposta (A, B ou C):");
+    } else {
+      alert("Resposta inválida! Digite A, B ou C");
+      respostaJogador = prompt("Digite a resposta (A, B ou C):");
+    }
+  }
+  return (location = "./gameover.html");
+}
+
+// FASE 2 - Homem Aranha
+
+function confereResposta1(respostaCerta) {
+  var respostaJogador = prompt("Digite a resposta (A, B, ou C):");
+  var contador = 3;
+
+  while (contador >= 1) {
+    if (respostaJogador.toUpperCase() === respostaCerta) {
+      alert("Parabéns! Resposta correta!");
+      return (location = "./homemaranha2.html");
+    } else if (
+      respostaJogador.toUpperCase() === "A" ||
+      respostaJogador.toUpperCase() === "C"
+    ) {
+      contador--;
+      alert(
+        `Resposta incorreta, tente novamente!\n Tentativas restantes: ${contador}`
+      );
+      respostaJogador = prompt("Digite a resposta (A, B ou C):");
+    } else {
+      alert("Resposta inválida! Digite A, B ou C");
+      respostaJogador = prompt("Digite a resposta (A, B ou C):");
+    }
+  }
+  return (location = "./gameover.html");
+}
